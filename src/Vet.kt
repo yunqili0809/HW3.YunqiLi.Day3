c645cffa792850c6736132c66d097af11f1cf161
yunqili0809 <42676224+yunqili0809@users.noreply.github.com>
2019-07-26 14:15:08 -0500

    interface

--- src/Vet.kt
+++ src/Vet.kt
@@ -1,3 +1,8 @@
+class Vehicle: Roamable {
+    override fun roam() {
+        println("The Viehcle is roaming")
+    }
+}
 class Vet(){
     fun giveShoot(animal: Animal){
         animal.makeNoise()
